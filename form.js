01a95af1776aded231aea63e1a24d3bb48504e16
Tyler Star <binglux@gmail.com>
2019-07-08 15:02:52 +0800

    Update form.js

--- form.js
+++ form.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 export const useSignUpForm = (initialState, callback) => {
+  // It's critical to initalise the state even they're empty
+  // More details: https://reactjs.org/docs/forms.html#controlled-components
+  // Explanation: https://stackoverflow.com/questions/37427508/react-changing-an-uncontrolled-input
   const [inputs, setInputs] = useState(initialState);
 
   const handleSubmit = (event) => {
